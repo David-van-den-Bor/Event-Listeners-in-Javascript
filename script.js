e3cf6b2f6b08a36cf8f0aae0f50f03f633eb0e17
David van den Bor <david@davidvandenbor.nl>
2021-03-01 21:50:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ document.addEventListener("click", function (event) {
 document.addEventListener("mouseover", function (event) {
 	if (event.target.matches(".hello")) {
 		document.querySelector(".post-it").textContent =
-			"The Mouse HOVERS on Hello!";
+			"Mouse HOVERS on Hello!";
 		return;
 	}
 	if (event.target.matches(".goodbye")) {
 		document.querySelector(".post-it").textContent =
-			"The mouse HOVERS on Goodbye!";
+			"Mouse HOVERS on Goodbye!";
 		return;
 	}
 });
