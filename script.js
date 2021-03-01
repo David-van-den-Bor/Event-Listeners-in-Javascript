aa46ffc31487f372fabb2932b49a37452e978c5c
David van den Bor <david@davidvandenbor.nl>
2021-03-01 21:48:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-
-
 /* combined event listeners! */
-
 
 /* ONE TO LISTEN FOR ALL CLICKS ON HTML ELEMENTS! */
 
-document.addEventListener('click', function (event) {
-
-	if (event.target.matches('.hallo')) {
-		 	document.querySelector(".notitie").textContent = "Je hebt GEKLIKT op: Hallo!";
+document.addEventListener("click", function (event) {
+	if (event.target.matches(".hello")) {
+		document.querySelector(".post-it").textContent = "You clicked on: Hello!";
 		return;
 	}
-	if (event.target.matches('.totziens')) {
-			document.querySelector(".notitie").textContent = "Je hebt GEKLIKT op: Tot ziens!";
+	if (event.target.matches(".goodbye")) {
+		document.querySelector(".post-it").textContent = "You clicked on: Goodbye!";
 		return;
 	}
-
 });
 
 /* ONE TO LISTEN FOR ALL MOUSEOVERS ON HTML ELEMENTS! */
 
-document.addEventListener('mouseover', function (event) {
-
-	if (event.target.matches('.hallo')) {
-		 	document.querySelector(".notitie").textContent = "De muis is OVER Hallo!";
+document.addEventListener("mouseover", function (event) {
+	if (event.target.matches(".hello")) {
+		document.querySelector(".post-it").textContent =
+			"The Mouse HOVERS on Hello!";
 		return;
 	}
-	if (event.target.matches('.totziens')) {
-			document.querySelector(".notitie").textContent = "De muis is OVER Tot ziens!";
+	if (event.target.matches(".goodbye")) {
+		document.querySelector(".post-it").textContent =
+			"The mouse HOVERS on Goodbye!";
 		return;
 	}
-
 });
